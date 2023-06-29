379960e85f34c85a96747dd7000427473c4585bb
IvanTaranenko <vanokalipsi@gmail.com>
2023-06-29 16:49:19 +0300

    first commit

---
+++ app/Models/Post.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'title',
+        'body',
+        'user_id',
+    ];
+
+    public function author(){
+        return $this->belongsTo('App/User');
+    }
+    public function post_images(){
+        return $this->hasMany('App/PostImage');
+    }
+
+
+
+}
